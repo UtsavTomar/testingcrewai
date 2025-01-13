e06a1cb35b35d148e505abfed04e57ffe97b197b
UtsavTomar <utsav.xalt@gmail.com>
2025-01-13 17:06:35 +0530

    Update main.py

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -3,7 +3,7 @@ from my_project.crew import SimpleAgentCrew
 def run():
 
     inputs = {
-        "text": """Data Science is an interdisciplinary field that combines scientific methods, processes, algorithms, and systems to extract knowledge and insights from structured and unstructured data. It involves the use of mathematics, statistics, specialized programming, advanced analytics, artificial intelligence (AI), and machine learning to uncover actionable insights hidden in an organization’s data"""
+        "text": "text"
     }
     # Kicking off the crew
     result = SimpleAgentCrew().crew().kickoff(inputs=inputs)
