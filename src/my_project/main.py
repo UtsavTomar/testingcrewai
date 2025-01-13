e13358d03788ca176779b4937f7be8f78e1b5d63
UtsavTomar <utsav.xalt@gmail.com>
2025-01-13 18:13:47 +0530

    Update main.py

--- src/my_project/main.py
+++ src/my_project/main.py
@@ -3,11 +3,12 @@ from my_project.crew import SimpleAgentCrew
 def run():
 
     inputs = {
-        "text": "text"
+        "text": "he concept of data in the context of computing has its roots in the work of Claude Shannon, an American mathematician known as the father of information theory. He ushered in binary digital concepts based on applying two-value Boolean logic to electronic circuits. Binary digit formats underlie the CPUs, semiconductor memories and disk drives, as well as many of the peripheral devices common in computing today. Early computer input for control and data took the form of punch cards, followed by magnetic tape and the hard disk."
     }
     # Kicking off the crew
     result = SimpleAgentCrew().crew().kickoff(inputs=inputs)
 
     # Save the summary result as a Markdown file
-    with open("summary.md", "w") as f:
-        f.write(result.raw)
+    pdf = MarkdownPdf(toc_level=1)
+    pdf.add_section(Section(result.raw))
+    pdf.save("inputs_sales_pdf.pdf")
